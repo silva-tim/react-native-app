53623bcfc9563061e87121bbc991cbe768282434
Tim Silva <tim17silva@gmail.com>
2023-08-30 19:22:06 +0000

    Added buttons

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,28 +1,37 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Pressable } from 'react-native';
 
 export default function Home() {
   return (
-    <View style={{ backgroundColor: '#31B6A8', height: 350, paddingTop: 50 }}>
-      <View style={styles.view}>
-        <Text style={styles.text}>Income</Text>
-        <Text style={styles.text}>Expenses</Text>
-      </View>
-      <View style={styles.view}>
-        <Text style={styles.text}>$5,000</Text>
-        <Text style={styles.text}>$4,500 of $5,000</Text>
-      </View>
-      <View
-        style={{
-          height: 200,
-          flexDirection: 'row',
-          gap: 50,
-          alignItems: 'flex-end',
-          paddingHorizontal: 40,
-        }}
-      >
-        <View style={styles.bars}></View>
-        <View style={styles.bars}></View>
-      </View>
+    <View style={{ backgroundColor: 'white', height: 1000 }}>
+      <ScrollView>
+        <View
+          style={{ backgroundColor: '#31B6A8', height: 350, paddingTop: 50 }}
+        >
+          <View style={styles.view}>
+            <Text style={styles.text}>Income</Text>
+            <Text style={styles.text}>Expenses</Text>
+          </View>
+          <View style={styles.view}>
+            <Text style={styles.text}>$5,000</Text>
+            <Text style={styles.text}>$4,500 of $5,000</Text>
+          </View>
+          <View style={styles.barsContainer}>
+            <View style={styles.bars} />
+            <View style={styles.bars} />
+          </View>
+        </View>
+        <View>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Add Expense</Text>
+          </Pressable>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Add Income</Text>
+          </Pressable>
+          <Text style={{ textAlign: 'center', fontSize: 25, paddingTop: 10 }}>
+            Recent Changes
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -43,5 +52,24 @@ const styles = StyleSheet.create({
     flex: 1,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+  },
+  barsContainer: {
+    height: 200,
+    flexDirection: 'row',
+    gap: 50,
+    alignItems: 'flex-end',
+    paddingHorizontal: 40,
+  },
+  button: {
+    backgroundColor: '#31B6A8',
+    height: 60,
+    marginHorizontal: 10,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 25,
+    color: 'white',
   },
 });
